ff0c7b9236b35f72b0e4cb64f43a64aabd439920
jithujp299 <jithujp299@gmail.com>
2025-03-02 17:56:46 +0530

    latest

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {motion} from 'framer-motion'
-import Tilt from 'react-tilt'
+// import Tilt from 'react-tilt'
+import Tilt from 'react-parallax-tilt';
 import {styles} from '../styles'
 import {services} from '../constants'
 import {fadeIn, textVariant} from '../utils/motion'
